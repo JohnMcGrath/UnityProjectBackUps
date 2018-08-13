fca03547685875247aaddde81a9d8fd683fee1cf
JohnMcGrath <mcgrathjohnjerome@gmail.com>
2018-08-13 15:46:23 +0100

    Initial Upload

---
+++ DriveTestCardboard/Assets/Resources/Scripts/vrRotate.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class vrRotate : MonoBehaviour {
+
+    public float angleOfRotation = 45;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+   public void RotateLeft()
+    {
+        transform.Rotate(Vector3.up, -angleOfRotation);
+    }
+
+   public void RotateRight()
+    {
+        transform.Rotate(Vector3.up, angleOfRotation);
+    }
+}
